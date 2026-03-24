a52c02fa237585f428091a39057a3bdf633c7d7b
phstolpe <123520279+phstolpe@users.noreply.github.com>
2026-03-24 15:30:34 +0100

    feat: add db population script and schema. --warning: the script drops existing tables

--- db/populatedb.js
+++ db/populatedb.js
@@ -4,9 +4,18 @@ const { Client } = require("pg");
 
 
 const SQL = `
+DROP TABLE IF EXISTS items;
+DROP TABLE IF EXISTS categories;
+
 CREATE TABLE IF NOT EXISTS categories(
   id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,	
-name VARCHAR ( 255 )
+   name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS items(
+  id INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,	
+  name TEXT NOT NULL,
+  category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE ON UPDATE CASCADE
 );
 
 INSERT INTO categories (name) 
@@ -14,12 +23,24 @@ VALUES
   ('Vegetables'),
   ('Protein'),
   ('Carbs');
+
+INSERT INTO items(name, category_id) 
+VALUES
+  ('Cucumber', 1),
+  ('Tofu', 2),
+  ('Rice', 3);
 `;
 
 async function main() {
+
+	const connectionString = process.argv[2];
+	if (!connectionString) {
+		console.error("No database connection string. Please provide one.");
+		process.exit(1);
+	}
 	console.log("seeding...");
 	const client = new Client({
-		connectionString: process.env.CONN
+		connectionString: connectionString
 	});
 	await client.connect();
 	await client.query(SQL);
